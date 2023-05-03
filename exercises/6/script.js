8434efe79105869341a8b70095e0cccf18de0f9f
alejoevilches <alejoevilches@gmail.com>
2023-05-03 15:51:01 -0300

    finishing exercise 6

--- exercises/6/script.js
+++ exercises/6/script.js
@@ -34,3 +34,31 @@ let ejercicio2=()=>{
 
 console.log("Resultado del ejercicio 2");
 console.log(ejercicio2())
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+let ejercicio3=()=>{
+    return comments.find(comment=>{
+        return comment.id==823423
+    })
+}
+
+console.log("Resultado del ejercicio 3")
+console.log(ejercicio3())
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+
+let ejercicio4=()=>{
+    comments.splice(comments.findIndex(comment=>{
+        return comment.id==823423
+    }),
+    1)
+    return comments
+}
+
+console.log("Resultado del ejercicio 4")
+console.log(ejercicio4())
