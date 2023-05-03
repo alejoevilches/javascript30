9e8d204b83810485824a56d3dcf76d8c079e858f
alejoevilches <alejoevilches@gmail.com>
2023-05-03 13:30:36 -0300

    now the function encontrarCiudades is fully working

--- exercises/5/script.js
+++ exercises/5/script.js
@@ -1,7 +1,15 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 let cities=[];
 
+//obtain data and push it to cities
 fetch(endpoint)
-    .then(e=>e.json())
-    .then(data=>cities.push(...data));
+.then(response=>response.json())
+.then(data=>cities.push(...data));
+
+function encontrarCiudades(palabra){
+    return cities.filter(ciudad=>{
+        let regex=new RegExp(palabra, "gi");
+        return ciudad.city.match(regex) || ciudad.state.match(regex); 
+    })
+}
 
