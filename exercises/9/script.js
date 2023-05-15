c6e5b891a995643ec33ed1202a83297191085181
alejoevilches <alejoevilches@gmail.com>
2023-05-15 15:35:45 -0300

    finishing exercise 9

--- exercises/9/script.js
+++ exercises/9/script.js
@@ -1,4 +1,4 @@
-const checkboxes=document.querySelectorAll(".item");
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
 let lastChecked;
 let inBetween;
@@ -8,27 +8,20 @@ checkboxes.forEach(checkbox => {
 });
 
 function handleCheck(e) {
-    // Check if they had the shift key down
-    // AND check that they are checking it
     let inBetween = false;
     if (e.shiftKey && this.checked) {
-      // go ahead and do what we please
-      // loop over every single checkbox
-      checkboxes.forEach(checkbox => {
-        console.log(checkbox);
-        if (checkbox === this || checkbox === lastChecked) {
-          inBetween = !inBetween;
-          console.log('Starting to check them in between!');
-        }
-  
-        if (inBetween) {
-          checkbox.checked = true;
-        }
-      });
+     // loop over every single checkbox
+     checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+      inBetween = !inBetween;
+      }
+      if (inBetween) {
+      checkbox.checked = true;
+      }
+     });
     }
-  
-    lastChecked = this;
-  }
+   lastChecked = this;
+   }
 
 
 
