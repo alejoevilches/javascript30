5b896e005039db488f1d860782fdf7f80ea52dc4
alejoevilches <alejoevilches@gmail.com>
2023-05-15 15:29:34 -0300

    testing app

--- exercises/9/script.js
+++ exercises/9/script.js
@@ -7,12 +7,28 @@ checkboxes.forEach(checkbox => {
     checkbox.addEventListener("click",handleCheck)
 });
 
-function handleCheck(e){
-    inBetween=false;
-    if (e.shiftKey && this.checked){
-        checkboxes.forEach(checkbox=>{
-            console.log(checkbox)
-        })
+function handleCheck(e) {
+    // Check if they had the shift key down
+    // AND check that they are checking it
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+      // go ahead and do what we please
+      // loop over every single checkbox
+      checkboxes.forEach(checkbox => {
+        console.log(checkbox);
+        if (checkbox === this || checkbox === lastChecked) {
+          inBetween = !inBetween;
+          console.log('Starting to check them in between!');
+        }
+  
+        if (inBetween) {
+          checkbox.checked = true;
+        }
+      });
     }
-}
+  
+    lastChecked = this;
+  }
+
+
 
