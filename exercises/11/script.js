96a083fc1b5da27174f35dc45b9b9c0b5e3e3263
alejoevilches <alejoevilches@gmail.com>
2023-05-16 15:08:05 -0300

    finishing project 11

--- exercises/11/script.js
+++ exercises/11/script.js
@@ -3,6 +3,13 @@ const secretWord="river"
 
 window.addEventListener("keyup", (e)=>{
     pressArray.push(e.key)
-    pressArray.join("") == secretWord ? alert("Usaste la palabra secreta") : "0"
+    pressArray.splice(-secretWord.length-1, pressArray.length-secretWord.length);
+    checkSecretWord();
 })
+
+function checkSecretWord(){
+    if (pressArray.join("")==secretWord){
+        alert("Lo hicimos")
+    }
+}
 
