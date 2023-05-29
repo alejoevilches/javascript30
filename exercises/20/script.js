95d5555cdcb6d59f60812eb5336a042f6ec4ed89
alejoevilches <alejoevilches@gmail.com>
2023-05-29 00:17:03 -0300

    finishing exercise 20

--- exercises/20/script.js
+++ exercises/20/script.js
@@ -4,11 +4,18 @@ highlight.classList.add("highlight");
 document.body.append(highlight)
 
 function highlightLink(){
-    console.log("highlight");
+    const coords=this.getBoundingClientRect();
+    const coordsObj={
+        width:coords.width,
+        height:coords.height,
+        top:coords.top+window.scrollY,
+        left:coords.left+window.scrollX
+    };
+    highlight.style.width = `${coordsObj.width}px`;
+    highlight.style.height = `${coordsObj.height}px`;
+    highlight.style.transform = `translate(${coordsObj.left}px, ${coordsObj.top}px)`;
 }
 
 triggers.forEach(trigger=>{
-    trigger.addEventListener("mouseover",(e)=>{
-        console.log("highlight")
-    })
-});
+    trigger.addEventListener("mouseover", highlightLink)
+})
